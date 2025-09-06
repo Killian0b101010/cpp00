cbad5b4b86b6d4ed915a72e79cad9c35d32da48a
Killian0b101010 <teixeira.killian44600@gmail.com>
2025-09-06 22:59:43 +0200

    maj phonebook

--- ex01/Phonebook.class.hpp
+++ ex01/Phonebook.class.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #ifndef PHONEBOOK_CLASS_HPP
 #define PHONEBOOK_CLASS_HPP
 
@@ -9,58 +10,17 @@ class Contact
   std::string firstName;
   std::string lastName;
   std::string phoneNumber;
-
   public:
   // Constructeur
   Contact();
   // Destructeur
   ~Contact();
-
   // Parsing
   bool isValidPhone(const std::string& phoneNumber);
   bool isValidString(const std::string& name);
-  
   //Contact 
-  void contactSet()
-  {
-    do {
-    std::cout << "Please, enter First Name : ";
-    std::getline(std::cin,firstName);
-    if(!isValidString(firstName) || firstName.empty())
-    {
-      std::cout << "❌First Name : " << firstName;
-      std::cout << " is not valid❌" << std::endl;
-    }
-    }while(!isValidString(firstName) || firstName.empty());
-
-    do {
-    std::cout << "Please, enter Last Name : ";
-    std::getline(std::cin,lastName);
-    if(!isValidString(lastName) || lastName.empty())
-    {
-      std::cout << "❌Last Name : " << lastName;
-      std::cout << " is not valid❌" << std::endl;
-    }
-    }while(!isValidString(lastName) || lastName.empty());
-
-    do {
-    std::cout << "Please, enter Phone Number : ";
-    std::getline(std::cin,phoneNumber);
-    if(!isValidPhone(phoneNumber) || phoneNumber.empty())
-    {
-      std::cout << "❌Number : " << phoneNumber;
-      std::cout << " is not valid❌" << std::endl;
-    }
-    }while(!isValidPhone(phoneNumber) || phoneNumber.empty());
-
-    return;
-  }
-
+  void contactSet();
   // Print le PhoneBook
-  void contactDisplay()const
-  {
-    std::cout << firstName << " " << lastName << " : " << phoneNumber << std::endl;
-    return;
-  } 
+  void contactDisplay()const;
 };
 #endif
--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -2,25 +2,29 @@
 
 Contact::Contact(void)
 {
-  this->firstName = "";
-  this->lastName = "";
-  this->phoneNumber = "";
+  this -> firstName = "";
+  this -> lastName = "";
+  this -> phoneNumber = "";
   std::cout << "Constructor called" << std::endl;
   return;
 }
 
-Contact::~Contact(void){
+Contact::~Contact(void)
+{
   std::cout << "Destructor called" << std::endl;
   return;
 }
+
 static bool isChar(char c)
 {
   return std::isalpha(c);
 }
+
 bool Contact::isValidString(const std:: string &name)
 {
   return std::all_of(name.begin(),name.end(),isChar);
 }
+
 static bool isDigitChar(char c)
 {   
   return std::isdigit(c);
@@ -29,6 +33,46 @@ static bool isDigitChar(char c)
 bool Contact::isValidPhone(const std:: string &phoneNumber)
 {
   return std::all_of(phoneNumber.begin(), phoneNumber.end(), isDigitChar);
+}
+
+void Contact::contactSet()
+{
+    do {
+    std::cout << "Please, enter First Name : ";
+    std::getline(std::cin,firstName);
+    if(!isValidString(firstName) || firstName.empty())
+    {
+      std::cout << "❌First Name : " << firstName;
+      std::cout << " is not valid❌" << std::endl;
+    }
+    }while(!isValidString(firstName) || firstName.empty());
+
+    do {
+    std::cout << "Please, enter Last Name : ";
+    std::getline(std::cin,lastName);
+    if(!isValidString(lastName) || lastName.empty())
+    {
+      std::cout << "❌Last Name : " << lastName;
+      std::cout << " is not valid❌" << std::endl;
+    }
+    }while(!isValidString(lastName) || lastName.empty());
+
+    do {
+    std::cout << "Please, enter Phone Number : ";
+    std::getline(std::cin,phoneNumber);
+    if(!isValidPhone(phoneNumber) || phoneNumber.empty())
+    {
+      std::cout << "❌Number : " << phoneNumber;
+      std::cout << " is not valid❌" << std::endl;
+    }
+    }while(!isValidPhone(phoneNumber) || phoneNumber.empty());
+    return;
+}
+
+void Contact::contactDisplay()const
+{
+    std::cout << firstName << " " << lastName << " : " << phoneNumber << std::endl;
+    return;
 }
 
 int main(void)
