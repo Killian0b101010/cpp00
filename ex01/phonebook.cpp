f097cfc62084594360f1ad481ff036e6e633dc05
Killian0b101010 <teixeira.killian44600@gmail.com>
2025-10-31 09:49:56 +0100

    last push avant correct

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,66 +6,49 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 15:02:51 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/10/11 15:02:54 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/10/31 09:33:52 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "phonebook.hpp"
 
-#include "Phonebook.class.hpp"
-
-Contact::Contact(void)
-{
+Contact::Contact(void) {
   this->firstName = "";
   this->lastName = "";
   this->phoneNumber = "";
-  this ->nickName = "";
+  this->nickName = "";
   this->darkSecret = "";
   std::cout << "Constructor called" << std::endl;
 }
 
-Contact::~Contact(void)
-{
-  std::cout << "Destructor called" << std::endl;
-}
+Contact::~Contact(void) { std::cout << "Destructor called" << std::endl; }
 
-static bool isChar(char c) 
-{ 
-  return std::isalpha(c); 
-}
+static bool isChar(char c) { return std::isalpha(c); }
 
-bool Contact::isValidString(const std::string &name)
-{
-  for (unsigned int i = 0; i < name.length(); i++)
-  {
+bool Contact::isValidString(const std::string &name) {
+  for (unsigned int i = 0; i < name.length(); i++) {
     if (!isChar(name[i]))
       return false;
   }
   return true;
 }
 
-static bool isDigitChar(char c) 
-{ 
-  return std::isdigit(c); 
-}
+static bool isDigitChar(char c) { return std::isdigit(c); }
 
-bool Contact::isValidPhone(const std::string &phoneNumber)
-{
-  for (unsigned int i = 0; i < phoneNumber.length(); i++)
-  {
+bool Contact::isValidPhone(const std::string &phoneNumber) {
+  for (unsigned int i = 0; i < phoneNumber.length(); i++) {
     if (!isDigitChar(phoneNumber[i]))
       return false;
   }
   return true;
 }
 
-void Contact::contactSet()
-{
+void Contact::contactSet() {
   do {
     std::cout << "Please, enter First Name : ";
     std::getline(std::cin, firstName);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
     if (!isValidString(firstName) || firstName.empty())
@@ -75,9 +58,8 @@ void Contact::contactSet()
   do {
     std::cout << "Please, enter Last Name : ";
     std::getline(std::cin, lastName);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
     if (!isValidString(lastName) || lastName.empty())
@@ -87,21 +69,20 @@ void Contact::contactSet()
   do {
     std::cout << "Please, enter Phone Number : ";
     std::getline(std::cin, phoneNumber);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
-    if (!isValidPhone(phoneNumber) || phoneNumber.empty() ||phoneNumber.length() < 2 || phoneNumber.length() >= 15)
+    if (!isValidPhone(phoneNumber) || phoneNumber.empty() ||
+        phoneNumber.length() < 2 || phoneNumber.length() >= 15)
       std::cout << "❌ Phone Number not valid ❌" << std::endl;
   } while (!isValidPhone(phoneNumber) || phoneNumber.empty());
-  
+
   do {
     std::cout << "Please, enter your Nickname : ";
     std::getline(std::cin, nickName);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
     if (nickName.empty())
@@ -111,9 +92,8 @@ void Contact::contactSet()
   do {
     std::cout << "Please, enter your dark secret : ";
     std::getline(std::cin, darkSecret);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
     if (darkSecret.empty())
@@ -121,34 +101,27 @@ void Contact::contactSet()
   } while (darkSecret.empty());
 }
 
-static void truncated(const std::string &str)
-{
+static void truncated(const std::string &str) {
   if (str.length() > 10)
     std::cout << std::setw(10) << str.substr(0, 9) + ".";
   else
     std::cout << std::setw(10) << str;
 }
 
-void Contact::initDisplay() const
-{
-  std::cout << std::setw(10) << "Index" << "|" << 
-  std::setw(10) << "First Name" << "|" << 
-  std::setw(10) << "Last Name" << "|" <<
-  std::setw(10) << "Nickname" << "|" <<
-  std::setw(10) << "Phone Number" << std::endl;
+void Contact::initDisplay() const {
+  std::cout << std::setw(10) << "Index" << "|" << std::setw(10) << "First Name"
+            << "|" << std::setw(10) << "Last Name" << "|" << std::setw(10)
+            << "Phone Number" << "|" << "Nickname" << std::endl;
 }
 
-void Contact::contactDisplayNotTrunked() const
-{
+void Contact::contactDisplayNotTrunked() const {
   std::cout << "First Name: " << firstName << std::endl;
   std::cout << "Last Name : " << lastName << std::endl;
   std::cout << "Phone Number: " << phoneNumber << std::endl;
   std::cout << "Nickname: " << nickName << std::endl;
-  std::cout << "Dark Secret: " << darkSecret << std::endl;
 }
 
-void Contact::contactDisplayTrunked(int index) const
-{
+void Contact::contactDisplayTrunked(int index) const {
   std::cout << std::setw(10) << index << "|";
   truncated(firstName);
   std::cout << "|";
@@ -160,16 +133,14 @@ void Contact::contactDisplayTrunked(int index) const
   std::cout << std::endl;
 }
 
-void PhoneBook::addContact()
-{
+void PhoneBook::addContact() {
   contacts[index].contactSet();
   index = (index + 1) % 8;
   if (count < 8)
     count++;
 }
 
-void PhoneBook::searchContact() const
-{
+void PhoneBook::searchContact() const {
   if (count == 0) {
     std::cout << "I don't have any contacts" << std::endl;
     return;
@@ -193,18 +164,15 @@ void PhoneBook::searchContact() const
     std::cout << "❌ Invalid index ❌" << std::endl;
 }
 
-int main()
-{
+int main() {
   PhoneBook book;
   std::string command;
 
-  while (true)
-  {
+  while (true) {
     std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
     std::getline(std::cin, command);
-    if(std::cin.eof())
-    {
-      std::cout<<"\nPas de EOF ICI VOYONS !\n";
+    if (std::cin.eof()) {
+      std::cout << "\nPas de EOF ICI VOYONS !\n";
       exit(1);
     }
     if (command == "ADD")
--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,132 +6,102 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 15:03:44 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/10/16 02:22:45 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/10/31 09:46:12 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-  int Account::_nbAccounts = 0;
-  int Account::_totalAmount = 0;
-  int Account::_totalNbDeposits = 0;
-  int Account::_totalNbWithdrawals = 0;
-  Account::Account(int initial_deposit)
-  {
-    _displayTimestamp();
-    _accountIndex = getNbAccounts();
-    _nbAccounts++;
-    _amount = initial_deposit;
-    _totalAmount+=_amount;
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+Account::Account(int initial_deposit) {
+  _displayTimestamp();
+  _accountIndex = getNbAccounts();
+  _nbAccounts++;
+  _amount = initial_deposit;
+  _totalAmount += _amount;
 
-    std::cout
-     << " index:" <<_accountIndex << ";" 
-     << "amount:" << _amount << ";" 
-     << "created" << std::endl; 
-  }
+  std::cout << " index:" << _accountIndex << ";"
+            << "amount:" << _amount << ";"
+            << "created" << std::endl;
+}
 
-  Account::~Account(void)
-  {
-    _displayTimestamp();
-    _totalAmount-= _amount;
-    _nbAccounts-=1;
-    std::cout
-     << " index:" <<_accountIndex<< ";" 
-     << "amount:" << _amount << ";" 
-     << "closed" << std::endl;
-  } 
-  weweeeeee:
-  int Account::getTotalAmount(void) { return _totalAmount; } 
+Account::~Account(void) {
+  _displayTimestamp();
+  _totalAmount -= _amount;
+  _nbAccounts -= 1;
+  std::cout << " index:" << _accountIndex << ";"
+            << "amount:" << _amount << ";"
+            << "closed" << std::endl;
+}
+int Account::getTotalAmount(void) { return _totalAmount; }
 
-  int Account::getNbAccounts(void) { return _nbAccounts; } 
+int Account::getNbAccounts(void) { return _nbAccounts; }
 
-  int Account::getNbDeposits(void) { return _totalNbDeposits; } 
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
 
-  int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; } 
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 
-  void Account::displayAccountsInfos( void ) {
-            _displayTimestamp();
-            std::cout 
-            <<" accounts:" << getNbAccounts() << ";" 
-            <<"total:" << getTotalAmount() << ";"
-            <<"deposits:" << getNbDeposits() << ";"
-            <<"withdrawals:" << getNbWithdrawals() << std::endl; }
+void Account::displayAccountsInfos(void) {
+  _displayTimestamp();
+  std::cout << " accounts:" << getNbAccounts() << ";"
+            << "total:" << getTotalAmount() << ";"
+            << "deposits:" << getNbDeposits() << ";"
+            << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
 
-  void Account::_displayTimestamp(void)
-  {
-    time_t timestamp;
-    time(&timestamp);
-    struct tm *timeInfos = localtime(&timestamp);
-    std::cout 
-    <<"[" 
-    << timeInfos->tm_year+1900 
-    << timeInfos->tm_mon 
-    << timeInfos->tm_mday 
-    << timeInfos->tm_hour 
-    << timeInfos->tm_min 
-    << timeInfos->tm_sec
-    << "]"; 
-  }
+void Account::_displayTimestamp(void) {
+  time_t timestamp;
+  time(&timestamp);
+  struct tm *timeInfos = localtime(&timestamp);
+  std::cout << "[" << timeInfos->tm_year + 1900 << timeInfos->tm_mon
+            << timeInfos->tm_mday << timeInfos->tm_hour << timeInfos->tm_min
+            << timeInfos->tm_sec << "]";
+}
 
-  void	Account::makeDeposit( int deposit )
-  {
-    _displayTimestamp();
-    std::cout 
-    << " index" << ":" << _accountIndex << ";" 
-    << "p_amount" << ":" << _amount << ";"
-    << "deposits" << ":" << deposit << ";";
- 
-      _amount += deposit;
-      _nbDeposits+=1;
-      _totalAmount+=deposit;
-      _totalNbDeposits+=1;
-    std::cout
-    << "amount" << ":" << _amount << ";"
-    << "nb_deposit" << ":" << _nbDeposits
-    << std::endl;
-  }
+void Account::makeDeposit(int deposit) {
+  _displayTimestamp();
+  std::cout << " index" << ":" << _accountIndex << ";"
+            << "p_amount" << ":" << _amount << ";"
+            << "deposits" << ":" << deposit << ";";
 
-	bool	Account::makeWithdrawal( int withdrawal )
-  {
-    _displayTimestamp();
-    std::cout 
-    << " index" << ":" << _accountIndex << ";"
-    << "p_amout" << ":" << _amount << ":";
-    if(_amount < withdrawal)
-    {
-      std::cout << "refused" << std::endl;
+  _amount += deposit;
+  _nbDeposits += 1;
+  _totalAmount += deposit;
+  _totalNbDeposits += 1;
+  std::cout << "amount" << ":" << _amount << ";"
+            << "nb_deposit" << ":" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+  _displayTimestamp();
+  std::cout << " index" << ":" << _accountIndex << ";"
+            << "p_amout" << ":" << _amount << ":";
+  if (_amount < withdrawal) {
+    std::cout << "refused" << std::endl;
+  } else {
+    std::cout << "withdrawal" << ":" << withdrawal << ";";
+    if (_amount >= withdrawal) {
+      _nbWithdrawals += 1;
+      _amount = withdrawal;
+      _totalAmount -= _amount;
+      _totalNbWithdrawals += 1;
+      std::cout << "amount" << ":" << _amount << ";"
+                << "nb_withdrawals" << ":" << _nbWithdrawals << std::endl;
+      return (true);
     }
-    else
-    {
-      std::cout << "withdrawal" << ":" << withdrawal << ";";
-      if( _amount >= withdrawal)
-      {
-        _nbWithdrawals+=1; 
-        _amount = withdrawal;
-        _totalAmount -=_amount;
-        _totalNbWithdrawals+=1;
-        std::cout
-        << "amount" << ":" << _amount << ";"
-        << "nb_withdrawals" << ":" << _nbWithdrawals
-        << std::endl;
-        return(true);
-      }
-    }
-    return(false);
-  }   
+  }
+  return (false);
+}
 
-	int		Account::checkAmount( void ) const
-  {
-    return(_amount);
-  }
-  
-  void  Account::displayStatus(void)const
-  {
-    _displayTimestamp();
-    std::cout  
-    << " index" << ":" << _accountIndex << ":" 
-    << "amount" << ":" << _amount << ":"
-    << "deposits" << ":" << _nbDeposits << ":"
-    << "withdrawals" << ":"<< _nbWithdrawals << 
-    std::endl;
-  }
+int Account::checkAmount(void) const { return (_amount); }
+
+void Account::displayStatus(void) const {
+  _displayTimestamp();
+  std::cout << " index" << ":" << _accountIndex << ":"
+            << "amount" << ":" << _amount << ":"
+            << "deposits" << ":" << _nbDeposits << ":"
+            << "withdrawals" << ":" << _nbWithdrawals << std::endl;
+}
